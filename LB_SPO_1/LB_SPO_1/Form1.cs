86861b3d4a8033b182abfee6a09954d84fd5e6d5
alexit777 <90395600+alexit777@users.noreply.github.com>
2021-09-16 18:44:32 +0300

    lb

---
+++ LB_SPO_1/LB_SPO_1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LB_SPO_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            foreach (DriveInfo d in allDrives)
+            {
+                textBox1.Text = d.Name;
+                textBox2.Text = d.DriveType.ToString();
+                if (d.IsReady == true)
+                {
+                    textBox3.Text = d.VolumeLabel.ToString();
+                    textBox4.Text = d.DriveFormat;
+                    textBox5.Text = d.AvailableFreeSpace.ToString();
+                    textBox6.Text = d.TotalFreeSpace.ToString();
+                    textBox7.Text = d.TotalSize.ToString();
+                }
+            }
+        }
+
+    }
+}
